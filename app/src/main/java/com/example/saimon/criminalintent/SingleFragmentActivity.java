24a51fb7e4d9db65733c123aef44f1975088bb44
saimon.494 <saimon.494@gmail.com>
2017-11-26 23:12:41 +0300

    App2, Criminal Intent, page 196

--- app/src/main/java/com/example/saimon/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/saimon/criminalintent/SingleFragmentActivity.java
@@ -1,11 +1,13 @@
 package com.example.saimon.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CriminalActivity extends AppCompatActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,9 @@ public class CriminalActivity extends AppCompatActivity {
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
     }
 }
+
