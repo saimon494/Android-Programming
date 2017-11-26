fefc129f8b79608f6ce04429379630a2fe23309a
saimon.494 <saimon.494@gmail.com>
2017-11-26 16:04:19 +0300

    Criminal Intent - page 166

---
+++ app/src/main/java/com/example/saimon/criminalintent/CrimeFragment.java
@@ -0,0 +1,15 @@
+package com.example.saimon.criminalintent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class CrimeFragment extends Fragment{
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+}
